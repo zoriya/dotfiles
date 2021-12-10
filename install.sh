a4722e21ed79749ccc21f366dbc350a00d8f1e02
Zoe Roux <zoe.roux@sdg.moe>
2021-12-10 21:09:51 +0100

    Cleaning up the zsh configuration and adding plugins

--- install.sh
+++ install.sh
@@ -1,8 +1,28 @@
 #!/usr/bin/env zsh
+set -e
 
-cd $($dirname $0)
+cd $(dirname $0)
 
-for file in $(find */ -type f -not '*.zsh'); do
-	echo $file
-	ln -s $file ~/.$(basename $file)
+info()
+{
+	printf "\r[ \033[00;34m..\033[0m ] $*\n"
+}
+
+warn()
+{
+	printf "\r[ \033[00;31m!!\033[0m ] $*\n"
+}
+
+for file in $(find */ -type f -not -name '*.zsh'); do
+	dest=~/.$(basename $file)
+	if [[ $(readlink -f $dest) == $file ]]; then
+		info File already linked ($file). Skipping...
+		break
+	fi
+	info Linking $file
+	if [[ -e $dest ]]; then
+		warn File $dest already exists. Moving it to ~/bak/
+		mv $dest ~/bak -f
+	fi
+	ln -s $(realpath $file) "$dest" -f
 done
