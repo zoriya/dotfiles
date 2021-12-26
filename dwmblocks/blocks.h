85344a54e7f2ae8d053745b7ba039dd5ecb92d0b
Zoe Roux <zoe.roux@sdg.moe>
2021-12-26 21:27:23 +0100

    Fixing dwmblock gitignore

---
+++ dwmblocks/blocks.h
@@ -0,0 +1,27 @@
+#ifndef BLOCKS_PATH
+	#define BLOCKS_PATH
+#endif
+
+#define PATH(event)  BLOCKS_PATH event
+
+
+static Block blocks[] = {
+/*    Icon  Command                      Update Interval  Update Signal */
+	{ "",   PATH("dunst.sh"),            120,             20 },
+//	{ "",   PATH("mail.sh"),             120,             23 },
+	{ "",   PATH("redshift.sh"),         120,             19 },
+//	{ "",   PATH("mpc"),                 240,             29 },
+//	{ "",   PATH("mconnect"),            120,             20 },
+	{ "",   PATH("cpu.sh"),              120,             18 },
+	{ "",   PATH("memory.sh"),           120,             17 },
+	{ "",   PATH("volume.sh"),           5,               16 },
+	{ "",   PATH("bluetooth.sh"),        120,             15 },
+	{ "",   PATH("network.sh"),          20,              14 },
+	{ "",   PATH("battery.sh"),          60,              13 },
+	{ "",   PATH("date.sh"),             240,             12 },
+	{ "",   PATH("time.sh"),             30,              11 },
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
