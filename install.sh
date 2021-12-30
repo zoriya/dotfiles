0b0da0b8db84670dbc5afcb8a6d33fb6e5d8f923
Zoe Roux <zoe.roux@sdg.moe>
2021-12-30 20:35:43 +0100

    Reworking the install script

--- install.sh
+++ install.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env zsh
 set -e
-
-if [[ "$1" == "-h" ]]; then
-	echo "Usage: $0 [-i]"
-	echo "\t-i: Clone dependencies (oh-my-zsh, powerlevel10k...)"
-	exit 0
-fi
-
 cd $(dirname $0)
 
 info()
@@ -19,54 +12,23 @@ warn()
 	printf "\r[ \033[00;31m!!\033[0m ] $*\n"
 }
 
-# TODO pacman/zsh.hook location.
-# for file in $(find */ -type f -not -name '*.zsh'); do
-# 	dest=~/.$(basename $file)
-# 	if [[ $(readlink -f $dest) == $file ]]; then
-# 		info File already linked ($file). Skipping...
-# 		break
-# 	fi
-# 	info Linking $file
-# 	if [[ -e $dest ]]; then
-# 		warn File $dest already exists. Moving it to ~/bak/
-# 		mv $dest ~/bak -f
-# 	fi
-# 	ln -s $(realpath $file) "$dest" -f
-# done
-
-# TODO clean up this huge chunk
-ln -s "$(realpath zsh/zshrc)" ~/.zshrc -f
-ln -s "$(realpath zsh/profile)" ~/.profile -f
-ln -s "$(realpath X11/xinitrc)" ~/.xinitrc -f
-ln -s "$(realpath X11/Xresources)" ~/.Xresources -f
-ln -s "$(realpath wallpapers)" ~/.wallpapers -f
-ln -s "$(realpath dunst/config)" ~/.config/dunst -f
-ln -s "$(realpath sxhkd/config)" ~/.config/sxhkd -f
-ln -s "$(realpath rofi/config)" ~/.config/rofi -f
-ln -s "$(realpath kitty/config)" ~/.config/kitty -f
-ln -s "$(realpath picom/config)" ~/.config/picom -f
-ln -s "$(realpath flameshot/config)" ~/.config/flameshot -f
-ln -s "$(realpath fontconfig/config)" ~/.config/fontconfig -f
-ln -s "$(realpath gtk/themes)" ~/.themes -f
-ln -s "$(realpath gtk/config/gtk-3.0/settings.ini)" ~/.config/gtk-3.0/settings.ini -f
-ln -s "$(realpath gtk/gtkrc-2.0)" ~/.gtkrc-2.0 -f
-
-./qt5ct/install.sh
-
-sudo ln -s "$(realpath startdwm)" /usr/bin/startdwm -f
-sudo ln -s "$(realpath dwm.desktop)" /usr/share/xsessions/dwm.desktop -f
-sudo ln -s "$(realpath fonts)" /usr/local/share/fonts -f
-sudo ln -s /etc/fonts/conf.avail/10-scale-bitmap-fonts.conf /etc/fonts/conf.d/
-
-clone()
-{
-	if [[ ! -d "$1" ]]; then
-		info "Clonning $(basename $1) ..."
-		git clone "$2" "$1"
-	fi
+usage() {
+	echo "Usage: $0 [-i]"
+	echo "\t-d: Clone dependencies (oh-my-zsh, powerlevel10k...)"
+	echo "\t-x: Install things for an X server (fonts, dwm, redshift...)"
+	echo "\t-h: Show this help message."
+	exit 0
 }
 
-if [[ "$1" == "-i" ]]; then
+dependencies() {
+	clone()
+	{
+		if [[ ! -d "$1" ]]; then
+			info "Clonning $(basename $1) ..."
+			git clone "$2" "$1"
+		fi
+	}
+	
 	info "Installing dependencies..."
 
 	local ZSH_CUSTOM=~/.oh-my-zsh/custom
@@ -82,5 +44,57 @@ if [[ "$1" == "-i" ]]; then
 		export ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
 		eof
 	fi
-fi
+}
+
+install() {
+	for topic in $(find . -mindepth 1 -maxdepth 1 -type d -not -name '.*'); do
+		if [[ ${topic##*.} == "ln" ]]; then
+			dest=~/.$(basename ${topic%.*})
+			[[ -f $dest ]] \
+				&& warn "$dest already exists." \
+				|| info "Linking $dest" && ln -s $(realpath $topic) $dest
+		elif [[ -f $topic/Makefile ]]; then
+			info "Running Makefile for $topic (commented for now)"
+			#make -C $topic install
+		elif [[ -f $topic/install.sh ]]; then
+			info "Running install.sh for $topic"
+			$topic/install.sh
+		else
+			for file in $(find $topic -type f -not -name '*.zsh' -or -type d -path '*.ln' -prune); do
+				dest=~/.$(realpath --relative-to $topic $file)
+				[[ -d $file ]] && dest=${dest%.*}
+
+				[[ $(readlink -f $dest) == $(realpath $file) ]] && break
+				info "Linking $file"
+				if [[ -e $dest ]]; then
+					warn "File $dest already exists. Moving it to ~/bak/"
+					mkdir -p ~/bak
+					mv $dest ~/bak -f
+				fi
+				ln -s $(realpath $file) "$dest" -f
+			done
+			# TODO support with or without X
+		fi
+	done
+}
+
+while getopts "dx" opt; do
+	case $opt in
+	d) dependencies ;;
+	x) echo "Not Implemented yet." ;;
+	*) usage ;;
+	esac
+done
+
+install
+
+# TODO pacman/zsh.hook location.
+
+# TODO clean up this huge chunk
+info "toto"
+sudo ln -s "$(realpath startdwm)" /usr/bin/startdwm -f
+sudo ln -s "$(realpath dwm.desktop)" /usr/share/xsessions/dwm.desktop -f
+sudo ln -s "$(realpath fonts)" /usr/local/share/fonts -f
+sudo ln -s /etc/fonts/conf.avail/10-scale-bitmap-fonts.conf /etc/fonts/conf.d/
+
 info "DONE."
--- qt5ct/install.sh
+++ qt5ct/install.sh
@@ -2,6 +2,7 @@
 set -e
 cd $(dirname $0)
 
-ln -s $(realpath qt5ct/qt5ct.conf) ~/.config/qt5ct/qt5ct.conf -f
+mkdir -p ~/.config/qt5ct
+ln -s $(realpath qt5ct.conf) ~/.config/qt5ct/qt5ct.conf -f
 ln -s $(realpath Material-Ocean/colors/Material-Ocean.conf) ~/.config/qt5ct/colors -f
 ln -s $(realpath Material-Ocean/colors/Material-Ocean.conf) ~/.config/qt5ct/colors -f
