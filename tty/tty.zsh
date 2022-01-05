b4d6269a961e89bd6dd51a0517345b96cdd00a35
Zoe Roux <zoe.roux@sdg.moe>
2022-01-05 10:18:49 +0100

    tty: configuring colors

---
+++ tty/tty.zsh
@@ -0,0 +1,20 @@
+if [ "$TERM" = "linux" ]; then
+    #echo -en "\e]P03b4252" #black
+    echo -en "\e]P84c566a" #darkgrey
+    echo -en "\e]P199343e" #darkred
+    echo -en "\e]P9bf616a" #red
+    echo -en "\e]P274be35" #darkgreen
+    echo -en "\e]PAa3be8c" #green
+    echo -en "\e]P3ffb521" #brown
+    echo -en "\e]PBebcb8b" #yellow
+    echo -en "\e]P42e98ff" #darkblue
+    echo -en "\e]PC81a1c1" #blue
+    echo -en "\e]P5a03c8e" #darkmagenta
+    echo -en "\e]PDb48ead" #magenta
+    echo -en "\e]P600ddff" #darkcyan
+    echo -en "\e]PE8fbcbb" #cyan
+    echo -en "\e]P7e5e0f0" #lightgrey
+    echo -en "\e]PFe7b6de" #white
+    clear #for background artifacting
+fi
+
