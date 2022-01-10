d169e6117d6b5cc6385340a57a0c42252a7a2ea2
Zoe Roux <zoe.roux@sdg.moe>
2022-01-10 20:47:18 +0100

    nvim: starting to configure lsp

--- nvim/config/nvim.ln/lua/user/plugins.lua
+++ nvim/config/nvim.ln/lua/user/plugins.lua
@@ -52,7 +52,6 @@ return packer.startup(function(use)
 	use "moll/vim-bbye"
 	use "nvim-lualine/lualine.nvim"
 	use "akinsho/toggleterm.nvim"
-	use "ahmedkhalf/project.nvim"
 	use "lewis6991/impatient.nvim"
 	use "lukas-reineke/indent-blankline.nvim"
 	use "goolord/alpha-nvim"
