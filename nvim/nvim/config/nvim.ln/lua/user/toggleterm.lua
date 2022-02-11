14a3ccf42ab79aa56b42714d9999015e32f303af
Zoe Roux <zoe.roux@sdg.moe>
2022-02-12 00:23:51 +0100

    Binding the escape key to got to normal mode inside the toggleterm

--- nvim/nvim/config/nvim.ln/lua/user/toggleterm.lua
+++ nvim/nvim/config/nvim.ln/lua/user/toggleterm.lua
@@ -27,16 +27,13 @@ toggleterm.setup({
 })
 
 function _G.set_terminal_keymaps()
+  local opts = {noremap = true}
+  vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
 end
 
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
 local Terminal = require("toggleterm.terminal").Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
-
-function _LAZYGIT_TOGGLE()
-	lazygit:toggle()
-end
 
 local node = Terminal:new({ cmd = "node", hidden = true })
 
@@ -48,12 +45,6 @@ local ncdu = Terminal:new({ cmd = "ncdu", hidden = true })
 
 function _NCDU_TOGGLE()
 	ncdu:toggle()
-end
-
-local htop = Terminal:new({ cmd = "htop", hidden = true })
-
-function _HTOP_TOGGLE()
-	htop:toggle()
 end
 
 local python = Terminal:new({ cmd = "python", hidden = true })
