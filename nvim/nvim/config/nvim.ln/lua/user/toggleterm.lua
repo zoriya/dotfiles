30da5b3f75caacc3e9090d5a76254ad0bb3b2195
Zoe Roux <zoe.roux@sdg.moe>
2022-02-14 19:33:21 +0100

    Fixing nvim term escape

--- nvim/nvim/config/nvim.ln/lua/user/toggleterm.lua
+++ nvim/nvim/config/nvim.ln/lua/user/toggleterm.lua
@@ -9,7 +9,7 @@ toggleterm.setup({
 	hide_numbers = true,
 	shade_filetypes = {},
 	shade_terminals = true,
-	shading_factor = 2,
+	shading_factor = 1,
 	start_in_insert = true,
 	insert_mappings = true,
 	persist_size = true,
@@ -28,7 +28,7 @@ toggleterm.setup({
 
 function _G.set_terminal_keymaps()
   local opts = {noremap = true}
-  vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', "<C-n>", [[<C-\><C-n>]], opts)
 end
 
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
