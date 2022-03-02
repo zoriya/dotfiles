12891a5344a6294766b4890ecd8d35a3acca2ff3
Zoe Roux <zoe.roux@sdg.moe>
2022-03-03 00:56:07 +0100

    Updating tree cwd

--- nvim/nvim/config/nvim.ln/lua/user/telescope.lua
+++ nvim/nvim/config/nvim.ln/lua/user/telescope.lua
@@ -79,10 +79,10 @@ telescope.setup {
 		},
 	},
 	pickers = {
-		find_files = {
-			find_command = { "fd", "--type", "f", "--strip-cwd-prefix" },
-			hidden = true,
-		},
+		-- find_files = {
+		-- 	find_command = { "fd", "--type", "f", "--strip-cwd-prefix" },
+		-- 	hidden = true,
+		-- },
 		-- Default configuration for builtin pickers goes here:
 		-- picker_name = {
 		--   picker_config_key = value,
