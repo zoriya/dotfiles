419b7ae049509382be8400abd109c17e4779ef79
Zoe Roux <zoe.roux@sdg.moe>
2022-03-20 22:31:09 +0100

    Handling build

--- nvim/nvim/config/new/lua/build/init.lua
+++ nvim/nvim/config/new/lua/build/init.lua
@@ -14,25 +14,22 @@ M.setup = function (config)
 	M.config = vim.tbl_deep_extend('keep', M.config, config)
 end
 
--- https://stackoverflow.com/questions/49907620/how-to-fuse-array-in-lua
-local function concatArray(a, b)
-	local result = {unpack(a)}
-	table.move(b, 1, #b, #result + 1, result)
-	return result
-end
-
 M.list_projs = function ()
 	local projs = {}
 
 	for _, adapter in pairs(M.adapters) do
 		for _, match in pairs(vim.fn.glob(adapter.pattern, false, true)) do
-			projs = concatArray(projs, adapter.list(match))
+			for _, proj in pairs(adapter.list(match)) do
+				proj.adapter = adapter
+				proj.source = match
+				table.insert(projs, proj)
+			end
 		end
 	end
 	return projs
 end
 
-M.select_proj = function ()
+M.select_proj = function (on_select)
 	local projs = M.list_projs()
 	vim.ui.select(projs, {
 		prompt = "Select a project",
@@ -45,9 +42,10 @@ M.select_proj = function ()
 			return icon .. " " .. proj.name
 		end
 	}, function (proj)
+		if not proj then return end
 		M.projects[vim.fn.getcwd()] = proj
-		if M.config.on_select then
-			M.config.on_select()
+		if on_select then
+			on_select()
 		end
 	end)
 end
@@ -57,7 +55,12 @@ M.get_project = function ()
 end
 
 M.build = function ()
-	vim.cmd(M.config.runner .. " " .. M.get_project().adapter.build())
+	local proj = M.get_project()
+	if not proj then
+		M.select_proj(M.build)
+		return
+	end
+	vim.cmd(M.config.runner .. " " .. proj.adapter.build(proj))
 end
 
 
