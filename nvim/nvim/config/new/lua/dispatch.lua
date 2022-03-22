1d8f4b530e05cc12421b7a4b65fe0d4a4822d588
Zoe Roux <zoe.roux@sdg.moe>
2022-03-22 17:56:01 +0100

    Adding build bindings

--- nvim/nvim/config/new/lua/dispatch.lua
+++ nvim/nvim/config/new/lua/dispatch.lua
@@ -4,7 +4,10 @@ vim.g["dispatch_no_maps"] = 1
 
 local wk = require("which-key")
 wk.register({
-	b = { "<cmd>Make<cr>", "Build project" },
+	b = { "<cmd>lua require('build').build()<cr>", "Build project" },
+	r = { "<cmd>lua require('build').run()<cr>", "Run project" },
+	s = { "<cmd>lua require('build').select_proj()<cr>", "Select project" },
+	d = { "<cmd>lua require('build').debug()<cr>", "Debug project" },
 }, {
 	prefix = "<leader>",
 })
