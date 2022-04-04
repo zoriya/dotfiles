21f00b95060b3976edafbac280faf35472647f74
Zoe Roux <zoe.roux@sdg.moe>
2022-04-04 19:27:36 +0200

    Removing the old nvim confiu

--- nvim/nvim/config/nvim.ln/lua/user/mergetool.lua
+++
@@ -1,8 +0,0 @@
-vim.g.mergetool_layout = 'lmr'
--- possible values: 'local' (default), 'remote', 'base'
-vim.g.mergetool_prefer_revision = 'base'
-
-local keymap = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
-keymap("n", "mh", "<cmd>MergetoolDiffExchangeLeft<cr>", opts)
-keymap("n", "ml", "<cmd>MergetoolDiffExchangeRight<cr>", opts)
