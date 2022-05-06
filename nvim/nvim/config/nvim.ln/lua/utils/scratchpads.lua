d8da03b699275ce2938fb8e1e16c26d3e41ba44d
Zoe Roux <zoe.roux-stg@nickel.eu>
2022-05-06 15:02:58 +0200

    Add scratchpad binding

--- nvim/nvim/config/nvim.ln/lua/utils/scratchpads.lua
+++ nvim/nvim/config/nvim.ln/lua/utils/scratchpads.lua
@@ -5,3 +5,10 @@ vim.g["codi#interpreters"] = {
 }
 
 vim.g["codi#virtual_text_pos"] = 90
+
+local wk = require "which-key"
+wk.register({
+	s = { "<cmd>CodiSelect<cr>", "Open a Sratchpad" },
+}, {
+	prefix = "<leader>",
+})
